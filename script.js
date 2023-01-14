8f33c836de1f4de5007590625f8cba5f9b8927ae
Ritika Joshi <ritikajoshi0114@gmail.com>
2023-01-14 21:18:30 +0800

    Added basic word of the day functionality

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("chrome extension ready to go")
+// window.addEventListener('mouseup', selectedWord)
+
+// function selectedWord() {
+//     let selectedText = window.getSelection()
+//     console.log(selectedText)
+// }
+
+
+async function fetchData() {
+    const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '7d8216ebb0msh38b7f2a7fe4e9f4p1e8b2ajsn7bc8bf122288',
+          'X-RapidAPI-Host': 'word-of-the-day2.p.rapidapi.com'
+        }
+      };
+      
+      const record = await fetch('https://word-of-the-day2.p.rapidapi.com/word/today', options)
+        // .then(response => response.json())
+        // .then(response => console.log(response))
+        // .catch(err => console.error(err));
+
+        const res = await record.json()
+        console.log(res[1].word)
+    document.getElementById("def").innerHTML = res[1].word
+    document.getElementById("mean").innerHTML = res[1].mean
+}
+
+fetchData()
+
